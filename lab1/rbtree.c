871c96d966af19ee014c4aa1cbfeef44a28621ca
Aleksey Gromov <gromov-aleksey@outlook.com>
2017-10-02 20:16:51 +0700

    Add files via upload

--- lab1/rbtree.c
+++ lab1/rbtree.c
@@ -10,28 +10,23 @@ struct rbtree *rbtree_left_rotate(struct rbtree *root, struct rbtree *node)
 	if (Printer)
 		printf("    Левый поворот\n");
 	struct rbtree *right = node->right;
-	/* Create node->right link */
 	node->right = right->left;
+	
 	// Если левый существует
 	if (right->left != NullNode)
 		right->left->parent = node;
 		// Создаем связь с родительским
-	/* Create right->parent link */
-	if (right != NullNode)
+	if (right != NullNode) 
 		right->parent = node->parent;
 		// Создаем связь с родительским
-	if (node->parent != NullNode)
-	// меняем местами
-	{
+	if (node->parent != NullNode) {
+		// меняем местами
 		if (node == node->parent->left)
 			node->parent->left = right;
 		else
 			node->parent->right = right;
-	}
-	else
-	{
-		root = right;
-	}
+	} else root = right;
+	// 
 	right->left = node;
 	if (node != NullNode)
 		node->parent = right;
@@ -44,24 +39,17 @@ struct rbtree *rbtree_right_rotate(struct rbtree *root, struct rbtree *node)
 	if (Printer)
 		printf("    Правый поворот\n");
 	struct rbtree *left = node->left;
-	/* Create node->left link */
 	node->left = left->right;
 	if (left->right != NullNode)
 		left->right->parent = node;
-	/* Create left->parent link */
 	if (left != NullNode)
 		left->parent = node->parent;
-	if (node->parent != NullNode)
-	{
+	if (node->parent != NullNode) {
 		if (node == node->parent->right)
 			node->parent->right = left;
-		else
-			node->parent->left = left;
-	}
-	else
-	{
-		root = left;
-	}
+		else node->parent->left = left;
+	} else root = left;
+
 	left->right = node;
 	if (node != NullNode)
 		node->parent = left;
@@ -71,60 +59,53 @@ struct rbtree *rbtree_right_rotate(struct rbtree *root, struct rbtree *node)
 
 // Востановление свойств красно-черного дерева  
 // после добавления нового узла
-// На вход получаем два дерева. Изначальное и новое.
-struct rbtree *rbtree_fixup_add(struct rbtree *root, struct rbtree *node)
-{
-    // Создаем дерево для хранения
+// На вход получаем два дерева. Изначальное и новое. 
+struct rbtree *rbtree_fixup_add(struct rbtree *root, struct rbtree *node) {
+
+	// Создаем дерево для хранения
+	// дяди. 
 	struct rbtree *uncle;
 	// Если родительский узел красный
-	while (node != root && node->parent->color == COLOR_RED)
-	// Если родительский элемент текущего 
+	while (node != root && node->parent->color == COLOR_RED) {
+		// Если родительский элемент текущего 
 		// находится слева от дедушки
-	{
-		if (node->parent == node->parent->parent->left)
-		{
-			// Получаем узел Дедушка???
+		if (node->parent == node->parent->parent->left) {
+
+			// Получаем узел "Дядя"
 			uncle = node->parent->parent->right;
+			
 			// Случай 1 - Если дядя красный 
 			// В 1 случае необходимо сменить цвет предков
-			if (uncle->color == COLOR_RED)
-			{
+			if (uncle->color == COLOR_RED) {
+				
 				// Меняем цвет отца и дяди на черный
-				node->parent->color = COLOR_BLACK;          //родительский
-				uncle->color = COLOR_BLACK;                 //дядин
-				node->parent->parent->color = COLOR_RED;    //дедушкин
+				node->parent->color = COLOR_BLACK; 		// родительский
+				uncle->color = COLOR_BLACK;				// Дядин
+				node->parent->parent->color = COLOR_RED;// дедушки
 				node = node->parent->parent;
 				if (Printer)
 					printf("    Изменение цветов\n");
-			}
-			else
-			{
-				// Случай 2 и 3 - Если Дядя черный
+           } else { // Случай 2 и 3 - Если Дядя черный
 				// Если текущее дерево справа от родительского
-				if (node == node->parent->right)
-				{
+				if (node == node->parent->right) {
 					// Сменить случай со 2 на 3. 
 					// Текущий узел становится родителем.
 					// Родительский становится левым дочерним.
-					node = node->parent;    //левый поворот
+					node = node->parent; // Происзводим левый поворот.
 					root = rbtree_left_rotate(root, node);
 				}
-				// Случай 3
+				// Случай 3 - текущий 
 				node->parent->color = COLOR_BLACK;
 				node->parent->parent->color = COLOR_RED;
-				// Правый поворот.
-				root = rbtree_right_rotate(root, node->parent->parent);
+				// Правый поворот. 
+				root = rbtree_right_rotate(root,node->parent->parent);
 				if (Printer)
 					printf("    Изменение цветов\n");
 			}
-		}
-		else
-		{
-			// Если родительский элемент текущего
+		} else { // Если родительский элемент текущего
 				 // находится справа от дедушки
 			uncle = node->parent->parent->left;
-			if (uncle->color == COLOR_RED)
-			{
+			if (uncle->color == COLOR_RED) {
 				// Случай 4 - Дядя - красный
 				// Меняемм цвета дяди, отца и деда.
 				node->parent->color = COLOR_BLACK;
@@ -133,20 +114,17 @@ struct rbtree *rbtree_fixup_add(struct rbtree *root, struct rbtree *node)
 				node = node->parent->parent;
 				if (Printer)
 					printf("    Изменение цветов\n");
-			}
-			else
-			{
+			} else {
 			// Дядя - черынй
-				if (node == node->parent->left)
-				{
-					node = node->parent;
-					// Правый поворот - Случай 5 превращаем в 6
-					root = rbtree_right_rotate(root, node);
-				}
-				// Случай 6 - Меняем цвета и делаем левый поворот
-				node->parent->color = COLOR_BLACK;
-				node->parent->parent->color = COLOR_RED;
-				root = rbtree_left_rotate(root, node->parent->parent);
+			if (node == node->parent->left) { // Случай 5
+				node = node->parent;
+				// Правый поворот - Случай 5 превращаем в 6
+				root = rbtree_right_rotate(root, node);
+			}
+			// Случай 6 - Меняем цвета и делаем левый поворот
+			node->parent->color = COLOR_BLACK;
+			node->parent->parent->color = COLOR_RED;
+			root = rbtree_left_rotate(root, node->parent->parent);
 				if (Printer)
 					printf("    Изменение цветов\n");
 			}
@@ -161,43 +139,41 @@ struct rbtree *rbtree_fixup_add(struct rbtree *root, struct rbtree *node)
 struct rbtree *rbtree_add(struct rbtree *root, int key, char *value)
 {
 	struct rbtree *node, *parent = NullNode;
+	
 	// Поиск листа для нового элемента. 
-	for (node = root; node != NullNode && node != NULL;)
-	{
+	for (node = root; node != NullNode && node != NULL; ){ 
 		parent = node;
 		if (key < node->key)
 			node = node->left;
 		else if (key > node->key)
 			node = node->right;
-		else
-			return root;
+		else return root;
 	}
+
 	// Выделяем память под дерево
 	node = malloc(sizeof(*node));
-	// Выделяем память под дерево
-	if (node == NULL)
+	
+	// Проверка выделения памяти 
+	if (node == NULL) 
 		return NULL;
-	// Заполняем значения дерева
+
+	// Заполняем значения дерева	
 	node->key = key;
 	node->value = value;
 	node->color = COLOR_RED;
 	node->parent = parent;
-	node->left = NullNode;             //листья пока пустые
-	node->right = NullNode;             //Надо восстанавливать
-	                                    //свойста к-ч деревьев
+	node->left = NullNode; 			// Листья пока пустые
+	node->right = NullNode;			// Надо восстанавливать
+									// свойства К-Ч деревьев.
+	
 	// Устанавливаем указатели из родительского
 	// в дочерний (добавляемый)
-	if (parent != NullNode)
-	{
+	if (parent != NullNode) {
 		if (key < parent->key)
 			parent->left = node;
-		else
-			parent->right = node;
-	}
-	else
-	{
-		root = node;
-	}
+		else parent->right = node;
+	} else root = node;
+	
 	// Востанавливаем свойства К-Ч деревьев, и его возвращаем. 
 	return rbtree_fixup_add(root, node);
 }
@@ -230,87 +206,84 @@ struct rbtree *rbtree_max(struct rbtree *root)
 struct rbtree *rbtree_transplant(struct rbtree *root, struct rbtree *u, struct rbtree *v)
 {
 	// Если u - корень
-	if ((u->parent == NULL) || (u->parent == NullNode))
-		root = v;
-	else if (u == u->parent->left)	// u - левое поддерево
-		u->parent->left = v;
-	else
-		u->parent->right = v;		// u - правое поддерево 
-	v->parent = u->parent;			// Меняем родителя
-	return root;
+	if (u->parent == NullNode)
+		root = v; 
+	else if (u == u->parent->left) // u - левое поддерево
+		u->parent->left = v; 
+	else u->parent->right = v; // u - правое поддерево 
+	// Меняем родителя
+	v->parent = u->parent;
 }
+
 
 // Функция востановления свойств красно-черных деревьев 
 // после уаления. На вход подается дерево и узел.
 struct rbtree *rbtree_fixup_delete(struct rbtree *root, struct rbtree *x)
 {
-	struct rbtree *w;
-	while ((x != root) && (x->color == COLOR_BLACK))
-	{
-		if (x == x->parent->left)
-		{
+    struct rbtree *w; 
+	
+	while ((x != root) && (x->color == COLOR_BLACK)) {
+		if (x == x->parent->left) { 
 			w = x->parent->right;
-			if (w->color == COLOR_RED)
-			{
+			// Если дядя красный
+			if (w->color == COLOR_RED) {// Случай 1
 				w->color = COLOR_BLACK;
-				x->parent->color = COLOR_RED;
+				x->parent->color = COLOR_RED; 
 				root = rbtree_left_rotate(root, x->parent);
-				w = x->parent->right;
+                w = x->parent->right;
+				w = x->parent->right; 
 			}
-			if ((w->left->color == COLOR_BLACK) && (w->right->color == COLOR_BLACK))
-			{
-				w->color = COLOR_RED;
-				x = x->parent;
-			}
-			else
-			{
-				if (w->right->color == COLOR_BLACK)
-				{
-					w->left->color = COLOR_BLACK;
-					w->color = COLOR_RED;
+			// Если оба черные
+			if ((w->left->color == COLOR_BLACK) && // Случай 2
+				(w->right->color == COLOR_BLACK)) {
+				w->color = COLOR_RED; 
+				x = x->parent; 
+			} else {
+				if (w->right->color == COLOR_BLACK) { // Случай 3
+					w->left->color = COLOR_BLACK; 
+					w->color = COLOR_RED; 
 					root = rbtree_right_rotate(root, w);
 					w = x->parent->right;
 				}
-				w->color = x->parent->color;
-				x->parent->color = COLOR_BLACK;
+				// Случай 4
+				w->color = x->parent->color; 
+				x->parent->color = COLOR_BLACK; 
 				w->right->color = COLOR_BLACK;
-				root = rbtree_left_rotate(root, x->parent);
-				x = root;
+				rbtree_left_rotate(root, x->parent); 
+				x = root;  
 			}
-		}
-		else
-		{
+			
+		} 
+		else { // Случаи 5-8
 			w = x->parent->left;
-			if (w->color == COLOR_RED)
-			{
+			if (w->color == COLOR_RED) {// Случай 5
 				w->color = COLOR_BLACK;
-				x->parent->color = COLOR_RED;
-				root = rbtree_right_rotate(root, x->parent);
-				w = x->parent->left;
+				x->parent->color = COLOR_RED; 
+				root = rbtree_right_rotate(root, w);
+				w = x->parent->left; 
 			}
-			if ((w->right->color == COLOR_BLACK) && (w->left->color == COLOR_BLACK))
-			{
-				w->color = COLOR_RED;
-				x = x->parent;
+			if ((w->right->color == COLOR_BLACK) && // Случай 6
+				(w->left->color == COLOR_BLACK)) {
+				w->color = COLOR_RED; 
+				x = x->parent; 
 			}
-			else
-			{
-				if (w->left->color == COLOR_BLACK)
-				{
-					w->right->color = COLOR_BLACK;
-					w->color = COLOR_RED;
-					root = rbtree_left_rotate(root, w);
+			else { 
+				if (w->left->color == COLOR_BLACK) { // Случай 7
+					w->right->color = COLOR_BLACK; 
+					w->color = COLOR_RED; 
+					root = rbtree_left_rotate(root, x->parent);
 					w = x->parent->left;
 				}
-				w->color = x->parent->color;
-				x->parent->color = COLOR_BLACK;
+				// Случай 8
+				w->color = x->parent->color; 
+				x->parent->color = COLOR_BLACK; 
 				w->left->color = COLOR_BLACK;
 				root = rbtree_right_rotate(root, x->parent);
 				x = root;
 			}
-		}
+		}	
 	}
-	x->color = COLOR_BLACK;
+	x->color = COLOR_BLACK;  
 	return root;
 }
 
